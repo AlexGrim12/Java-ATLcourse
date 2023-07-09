9963a7224416de081b462f30c1e92a2f7613f3fe
Alejandro Grimaldo <alejandrogrim12@gmail.com>
2023-07-09 15:17:48 -0600

    helloWorld

---
+++ helloWorld.java
@@ -0,0 +1,9 @@
+
+class helloWorld {
+    public static void main(String[] args) {
+        String name = "Alex";
+        name = "Leo";
+        name = "Alex";
+        System.out.println("Hello " + name);
+    }
+}
